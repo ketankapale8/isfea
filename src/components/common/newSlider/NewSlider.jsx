8bcef05f34250e84d58a18b81ba1f831e35c989f
ketankapale8 <ketan.kapale@gmail.com>
2023-11-06 18:41:30 +0530

    bgChanges!!

--- src/components/common/newSlider/NewSlider.jsx
+++ src/components/common/newSlider/NewSlider.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Fade } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
-import one from '../../../assets/backgrounds/bg46.jpg'
-import two from '../../../assets/Banner2.jpg'
-import three from '../../../assets/backgrounds/bg45.jpg'
-import four from '../../../assets/backgrounds/bg44.jpg'
+// import one from '../../../assets/backgrounds/bg46.jpg'
+// import two from '../../../assets/Banner2.jpg'
+// import three from '../../../assets/backgrounds/bg45.jpg'
+// import four from '../../../assets/backgrounds/bg44.jpg'
+
+import one from '../../../assets/VectorImages/One.jpg'
+import two from '../../../assets/VectorImages/Two.jpg'
+import three from '../../../assets/VectorImages/Three.jpg'
+import four from '../../../assets/VectorImages/Four.jpg'
+
+
+
+
 
 const fadeImages = [
   {
@@ -32,7 +41,7 @@ const Slideshow = () => {
       <Fade arrows={false}>
         {fadeImages.map((fadeImage, index) => (
           <div key={index} style={{position:'relative'}}>
-            <img style={{ width: '100%' , height:'820px' , objectFit:'cover'}} src={fadeImage.img} />
+            <img style={{ width: '100%' , height:'720px' , objectFit:'cover' ,}} src={fadeImage.img} />
             <h2 style={{position:'absolute', top: '60%' , left:'10%' , color:'white', fontSize:'2.5rem'}}>{fadeImage.caption}</h2>
           </div>
         ))}
