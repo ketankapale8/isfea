53d99c9a65e012cab41e5af6df260b096d48eda0
ketankapale8 <ketan.kapale@gmail.com>
2023-11-09 11:21:51 +0530

    RegulationChanges!

--- src/components/core/About/About.jsx
+++ src/components/core/About/About.jsx
@@ -8,6 +8,7 @@ import HomeLeft from '../../common/HomeLeft/HomeLeft';
 import HomeRight from '../../common/HomeRight/HomeRight';
 import BannerComp from '../../common/BannerComp/BannerComp';
 import BannerImg from '../../../assets/bgremoved/bg2.png'
+import Regulations from './Regulations/Regulations';
 // import BannerComp1 from '../../common/BannerComp1/BannerComp';
 
 
@@ -15,9 +16,7 @@ const About = () => {
   return (
     <div className='about'>
       <BannerComp img={BannerImg} title={"About Us"}/>
-      {/* <div className="aboutTitle" style={{paddingLeft:'45%' , fontSize:'1.3rem', fontWeight:'bold', paddingTop:'30px', paddingBottom:'30px'}}>
-        About ISFEA
-      </div> */}
+     
        <HomeLeft 
        title={"Global Partnerships"} desc={"At the International Sports Federation Education Alliances (ISFEA), we recognize the transformative power of global partnerships in advancing sports education on a worldwide scale. Our commitment to forging these partnerships is at the heart of our mission. We seek to collaborate with international sports federations, educational institutions, governmental bodies, and non-profit organizations to create a seamless network of expertise and resources that transcends geographical boundaries. Through strategic alliances, we aim to pool our collective knowledge, experience, and resources to develop innovative sports education programs, facilitate research initiatives, and promote best practices that benefit athletes, coaches, and sports administrators across the globe. "}
        img={One}
@@ -30,11 +29,13 @@ const About = () => {
         desc1={"We invest in research and data analysis to better understand the profound effects of sports on education and personal development. Through innovation, we continuously evolve our programs to maximize their effectiveness."}
       />
 
-<HomeLeft 
+      <HomeLeft 
        title={"Overall Sports Development"} desc={"At the International Sports Federation Education Alliances (ISFEA), our commitment to overall sports development encompasses a holistic approach to nurturing athletic talent, enhancing coaching excellence, and promoting sports administration proficiency. We believe that a well-rounded sports ecosystem is essential for the growth of the sporting community, and we strive to contribute to this by offering comprehensive support and resources. Through our initiatives, we aim to develop high-caliber athletes by providing access to top-tier sports education, training, and mentorship, enabling them to excel in their chosen disciplines. Moreover, we are dedicated to empowering coaches with cutting-edge knowledge and methodologies, ensuring they can provide the best guidance and support to athletes, thereby elevating the quality of coaching in the sports industry. "}
        img={Three}
        desc1={"ISFEA also recognizes the significance of effective sports administration in the success and sustainability of sports organizations. We work to equip sports administrators with the necessary skills, tools, and resources to manage sports programs, events, and institutions efficiently. By focusing on sports administration, we aim to enhance the overall management of sports activities, which is essential for the growth, sustainability, and ethical governance of the sports industry."}/>
+       <Regulations/>
     </div>
+    
   )
 }
 
---
+++ src/components/core/About/Regulations/Regulations.jsx
@@ -0,0 +1,155 @@
+import React , {useState} from 'react';
+import './regulations.scss';
+import dot from '../../../../assets/regulations/rec.png'
+
+const Regulations = () => {
+    const [showReg , setShowReg] = useState(false)
+    const regulationsArr = [
+        {
+            title : "Participants (Art. 1):",
+            desc : [
+                {
+                    para1 : "Athletes must reside or attend school in the city they represent.",
+                    para2 : "Age eligibility is defined: athletes must be 12 years old by January 1 of the Games year and still be 15 years old on December 31 of the Games year.",
+                    para3 : "Athletes can only participate in one sport."
+
+                }
+            ]
+        }, 
+
+        {
+            title : "International Children’s Games (Art. 2):",
+            desc : [
+                {
+                    para1 : "Can be either Summer or Winter Games.",
+                    para2 : " Summer Games must include athletics and swimming as obligatory sports.",
+                    para3 : "Winter Games must include skiing and an ice sport.",
+                    para4 : "Participation must involve at least five cities from a minimum of three countries, with four different sports being held."
+
+                }
+            ]
+        },
+
+        {
+            title : "Spirit of the Children’s Games (Art. 3):",
+            desc : [
+                {
+                    para1 : "Emphasis on fair play and cultural exchange.",
+                    para2 : "Discourages qualifying competitions and straight knockout formats.",
+                    // para3 : "Athletes can only participate in one sport."
+
+                }
+            ]
+        },
+        {
+            title : "Sport (Art. 4):",
+            desc : [
+                {
+                    para1 : "Only sports recognized by the Grading Committee should be offered.",
+                    para2 : " Regional sports can be considered as demonstration sports with the final decision resting with the ISFEA Committee.",
+                    para3 : "Certain sports, such as boxing, full-contact, motor sports, descent, and Super-G, are prohibited.",
+                    // para4 : "Participation must involve at least five cities from a minimum of three countries, with four different sports being held."
+
+                }
+            ]
+        },
+
+        {
+            title : "Specific Regulations (Art. 5):",
+            desc : [
+                {
+                    para1 : "Refers to International Sports Federations Rules and Regulations for specific sports.",
+                    para2 : " The Committee of the International Children’s Games delegates a technical commission member for each sports event.",
+                    para3 : "Organizers are responsible for comprehensive medical care for participants.",
+                    // para4 : "Participation must involve at least five cities from a minimum of three countries, with four different sports being held."
+
+                }
+            ]
+        },
+
+        {
+            title : "Applications to host the Games (Art. 6):",
+            desc : [
+                {
+                    para1 : "Applications should be submitted to the Committee of the International Children’s Games office at least two years before the proposed Games.",
+                    para2 : " Must follow the official ISFEA Bid form and process.",
+                    // para3 : "Organizers are responsible for comprehensive medical care for participants.",
+                    // para4 : "Participation must involve at least five cities from a minimum of three countries, with four different sports being held."
+
+                }
+            ]
+        },
+
+        {
+            title : "Opening and Closing of the International Children’s Games (Art. 9):",
+            desc : [
+                {
+                    para1 : "The Opening and Closing Ceremonies include specific elements such as the handover of the flag, lighting of the Olympic flame, and address by the organizers.",
+                    para2 : " Teams must line up under the flag of their city.",
+                    para3 : "The Closing Ceremony involves lowering the flag and presenting a Certificate of Appreciation to the host city.",
+                    // para4 : "Participation must involve at least five cities from a minimum of three countries, with four different sports being held."
+
+                }
+            ]
+        },
+
+        {
+            title : "Various (Art. 10):",
+            desc : [
+                {
+                    para1 : "Matters not specified in these regulations are decided by the Committee of the International Children’s Games.",
+                    para2 : " The Secretary General of the International Children’s Games assists organizers with questions related to the event.",
+                     para3 : "Organizers must provide a complete documentation report and related materials for archival purposes after the Games' conclusion.",
+                    // para4 : "Participation must involve at least five cities from a minimum of three countries, with four different sports being held."
+
+                }
+            ]
+        },
+    ]
+
+    function Toggle(){
+        if(showReg == false){
+            setShowReg(true)
+        }else{
+            setShowReg(false)
+        }
+    }
+  return (<div className='regulationsMain'>
+        <button onClick={()=>Toggle()}>Show Regulations</button>
+        {showReg && (
+        <div className='regulations'>
+                <h3 className="regulationsTitle">Organization Regulations</h3>
+                    {regulationsArr.map(item=>{
+                        return (
+                            <div className="regulationsContainer">
+                                <h4 className="containerTitle">{item.title}</h4>
+                                <div className="containerDesc">
+                                    {item.desc.map(content => {
+                                        return (
+                                            <>
+                                            
+                                                <p>{content?.para1}</p>
+                                                <p>{content?.para2}</p>
+                                                <p>{content?.para3}</p>
+                                                <p>{content?.para4}</p>
+                                            
+                                            </>
+
+                                            
+                                        )
+                                    })}
+                                </div>
+
+                            </div>
+                        )
+                    })}
+
+
+        </div>
+        )}
+  
+  </div>
+  )
+}
+
+export default Regulations
